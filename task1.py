e515a076615c61985944f5bd44849239638bbc9c
Ivan Ustyantsev <vangold915@gmail.com>
2022-12-06 16:23:19 +0700

    Completed task1

--- task1.py
+++ task1.py
@@ -4,3 +4,23 @@
 (выработка в часах*ставка в час) + премия. Для выполнения расчета для
 конкретных значений необходимо запускать скрипт с параметрами.
 """
+from sys import argv
+
+def get_salary(output, wage_rate, bonus):
+    """
+    Функция расчета заработной платы сотрудника
+    :param output: выработка в часах
+    :param wage_rate: ставка в час
+    :param bonus: премия
+    :return: зарплата
+    """
+    try:
+        salary = int(output) * int(wage_rate) + int(bonus)
+    except ValueError:
+        return 'Введены неверные значения'
+    return salary
+
+
+name, production, rate, premium = argv
+print(f'Заработная плата сотрудника: {get_salary(production, rate, premium)}')
+
